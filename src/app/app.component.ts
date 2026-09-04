a892a563d5ea1f7e58795c8b1140327c123f8157
austin <acmoudy@yahoo.com>
2026-09-04 13:11:56 -0400

    Designed layouts for all sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HabitListItemComponent } from "./habit-list-item/habit-list-item.component";
+import { AddHabitComponent } from './add-habit/add-habit.component';
+import { InspirationalQuoteComponent } from './inspirational-quote/inspirational-quote.component';
+import { ProfileBarComponent } from './profile-bar/profile-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HabitListItemComponent],
+  imports: [RouterOutlet, HabitListItemComponent, AddHabitComponent, InspirationalQuoteComponent, ProfileBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
