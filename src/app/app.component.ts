c3e77619ef388772a1984766afbe9370208a9730
austin <acmoudy@yahoo.com>
2026-09-22 19:04:52 -0400

    Added widget to add habit with new name and goal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, ElementRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HabitListItemComponent } from "./habit-list-item/habit-list-item.component";
 import { AddHabitComponent } from './add-habit/add-habit.component';
@@ -14,9 +14,35 @@ import { ProfileBarComponent } from './profile-bar/profile-bar.component';
 export class AppComponent {
   title = 'Growth';
   @ViewChild('container', {read: ViewContainerRef}) vcr!: ViewContainerRef;
+  widgetVisible = false;
+  @ViewChild('goalName') goalNameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('habitName') habitName!: ElementRef<HTMLInputElement>;
 
-  createHabitComponent(){
+  showHabitWidget() {
+    this.widgetVisible = true;
+  }
+
+  hideHabitWidget() {
+    this.widgetVisible = false;
+    this.goalNameInput.nativeElement.value = "";
+    this.habitName.nativeElement.value = "";
+  }
+
+  createHabitComponent(habit: string, goal: string){
     const cmpRef = this.vcr.createComponent(HabitListItemComponent);
     cmpRef.instance.selfRef = cmpRef;
+    if(goal.localeCompare("") == 0){
+      cmpRef.instance.goal = "Goal";
+    }else {
+      cmpRef.instance.goal = goal;
+    }
+
+    if(habit.localeCompare("") == 0){
+      cmpRef.instance.habit = "Habit";
+    }else {
+      cmpRef.instance.habit = habit;
+    }
+    this.hideHabitWidget();
+    
   }
 }
--- src/app/habit-list-item/habit-list-item.component.ts
+++ src/app/habit-list-item/habit-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentRef, Input } from '@angular/core';
+import { Component, ComponentRef, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-habit-list-item',
@@ -8,8 +8,20 @@ import { Component, ComponentRef, Input } from '@angular/core';
 })
 export class HabitListItemComponent {
   @Input() selfRef!: ComponentRef<HabitListItemComponent>;
+  @ViewChild('streakCheck') check!: ElementRef<HTMLInputElement>;
+  habit: string = "Habit";
+  goal: string = "goal";
+  streak: number = 0;
 
   deleteSelf() {
     this.selfRef.destroy();
+  }
+
+  addToStreak(){
+    if(this.check.nativeElement.checked){
+      this.streak = this.streak + 1;
+    }else {
+      this.streak = this.streak - 1;
+    }
   }
 }
